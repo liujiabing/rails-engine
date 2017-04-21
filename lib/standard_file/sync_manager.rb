255c8bcfc069f089d8f5c6fba914cb3c9fbfa498
Mo Bitar <me@bitar.io>
2017-04-21 11:00:32 -0500

    remove test value

--- lib/standard_file/sync_manager.rb
+++ lib/standard_file/sync_manager.rb
@@ -29,7 +29,7 @@ module StandardFile
       end
 
       # manage conflicts
-      min_conflict_interval = 1
+      min_conflict_interval = 20
 
       saved_ids = saved_items.map{|x| x.uuid }
       retrieved_ids = retrieved_items.map{|x| x.uuid }
